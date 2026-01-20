f6a51ddf98993a78e1705b814a4689d590984711
Hikmah <hikmatsokunbitolulope@gmail.com>
2026-01-20 14:27:11 +0100

    almost complete

---
+++ src/components/Display.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Display({ list }) {
+  console.log(list);
+
+  return (
+   <div className="display">
+     <ul>
+      {list.map((x, i) => (
+        <li key={i}>
+          <input type="checkbox" name="" id="" />
+          <span>{x.amount}</span>
+          <span>{x.item}</span>
+          {/* <span>&times;</span> */}
+          <button>✖</button>
+        </li>
+      ))}
+    </ul>
+   </div>
+  );
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Header from './Header'
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <h1>We help you remember everything you need for your trip ✈</h1>
+
+    </footer>
   )
 }
