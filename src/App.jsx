4982136a09e1131eed50284348ac7b4eb44b78e1
Hikmah <hikmatsokunbitolulope@gmail.com>
2026-01-20 12:39:50 +0100

    adding js and more css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
       <Main/>
-      
+      <Footer/>
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,40 @@
-export default function Form() {
-  return (
-    <form>
-      <div className="select_amount">
-        <p>What do you need for your 😍 trip?</p>
-        <select name="Amount">
-          <option value="">1</option>
-          <option value="">2</option>
-          <option value="">3</option>
-          <option value="">4</option>
-          <option value="">5</option>
-        </select>
-      </div>
+import { useState } from "react";
 
-      <div className="add_trip">
-        <input type="text" />
-        <button>Add</button>
+export default function Form() {
+  const [item, setItem] = useState("");
+  const [amount, setAmount] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log({ amount, item });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <div className="select_amount">
+          <p>What do you need for your 😍 trip?</p>
+          <select
+            name="Amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+
+        <div className="add_trip">
+          <input
+            type="text"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+          <button>Add</button>
+        </div>
       </div>
     </form>
   );
